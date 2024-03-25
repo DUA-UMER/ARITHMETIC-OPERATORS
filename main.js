7a03305fc6fe04d3dd89cdae9b71be896e199769
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-03-25 23:58:51 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+"use strict";
+// ARITHMETIC OPERATORS
+//ADDITION
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 10;
+let y = 5;
+let sum = x + y;
+console.log(sum);
+//SUBTRACTION
+let a = 50;
+let b = 15;
+let subtract = a - b;
+console.log(subtract);
+//MULTIPLY
+let k = 5;
+let L = 3;
+let multiply = k * L;
+console.log(multiply);
+//DIVIDE
+let o = 75;
+let p = 5;
+let divide = o / p;
+console.log(divide);
+//modulous
+let Q = 80;
+let R = 10;
+let modulous = Q % R;
+console.log(modulous);
+//EXPONENT ++,--
+//INCREMENT ++
+let s = 8;
+let t = 5;
+let increment = ++s;
+console.log(increment);
+//DECREMENT --
+let E = 21;
+let F = 25;
+let decrement = --F;
+console.log(decrement);
+//PRE DECREMENT (--H) && POST DECREMENT (H--)
+//pre decrement
+let H = 50;
+console.log(--H);
+//POST decrement
+let I = 80;
+console.log(I--);
+console.log(I--);
---
+++ main.ts
@@ -0,0 +1,63 @@
+// ARITHMETIC OPERATORS
+     //ADDITION
+
+let x:number = 10;
+let y:number = 5;
+
+let sum:number = x+y
+console.log(sum)
+
+  //SUBTRACTION
+
+  let a:number = 50;
+  let b:number = 15;
+
+  let subtract:number =a-b
+  console.log(subtract)
+
+       //MULTIPLY
+
+let k:number = 5;
+let L:number = 3;
+
+let multiply:number = k*L
+console.log(multiply)
+
+       //DIVIDE
+let o: number = 75;
+let p: number = 5;
+
+let divide: number = o/p
+console.log(divide)
+
+      //modulous
+let Q:number = 80;
+let R:number = 10;
+
+let modulous: number = Q%R
+console.log(modulous)
+
+
+    //EXPONENT ++,--
+    //INCREMENT ++
+
+let s:number = 8;
+let t:number = 5;
+let increment: number = ++s
+console.log(increment)
+
+       //DECREMENT --
+let E:number =21;
+let F:number =25;
+let decrement : number = --F
+console.log(decrement)
+
+   //PRE DECREMENT (--H) && POST DECREMENT (H--)
+
+//pre decrement
+let H:number = 50;
+console.log(--H)
+//POST decrement
+let I:number = 80;
+console.log(I--)
+console.log(I--)
